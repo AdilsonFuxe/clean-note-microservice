1991df944284b243db88ea86eb63abf15ff092b8
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-15 07:54:11 +0100

    test: ensure AddNoteController returns 400 if title is not provided in http request body

---
+++ __tests__/presentation/controllers/add-note-controller.spec.ts
@@ -0,0 +1,15 @@
+import { AddNoteController } from '@src/presentation/controllers';
+import { MissingParamError } from '@src/presentation/errors';
+import { badRequest } from '@src/presentation/helpers';
+
+describe('AddNoteController', () => {
+  it('Should return 400 if title is not provided in http request body', async () => {
+    const sut = new AddNoteController();
+    const httpResponse = await sut.handle({
+      body: {
+        description: 'any_description',
+      },
+    });
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('title')));
+  });
+});
