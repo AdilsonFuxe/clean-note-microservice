e857fca82a0cc170840944654c272e045f77cbbe
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-15 08:06:22 +0100

    feat: ensure AddNoteController returns calls AddNote with correct values

--- src/presentation/controllers/add-note-controller.ts
+++ src/presentation/controllers/add-note-controller.ts
@@ -4,15 +4,20 @@ import {
   HttpResponse,
 } from '@src/presentation/protocols';
 import { badRequest, serverError } from '@src/presentation/helpers';
-import { MissingParamError } from '../errors';
+import { MissingParamError } from '@src/presentation/errors';
+import { AddNote } from '@src/domain/usecases';
 
 export class AddNoteController implements Controller {
+  constructor(private readonly addNote: AddNote) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       for (const field of ['title', 'description']) {
         if (!httpRequest.body[field])
           return badRequest(new MissingParamError(field));
       }
+      const { title, description } = httpRequest.body;
+      await this.addNote.add({ title, description });
       return await Promise.resolve({ statusCode: 200 });
     } catch (error) {
       return serverError(error);
