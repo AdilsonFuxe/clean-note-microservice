a01cd67dd47434981ae6c6cf19ea8a525fa2d6d7
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-15 06:10:40 +0100

    refactor: move sut creation to a single factory method

--- __tests__/data/db-add-note.spec.ts
+++ __tests__/data/db-add-note.spec.ts
@@ -3,22 +3,36 @@ import { DbAddNote } from '@src/data/usecases';
 import { Note } from '@src/domain/models';
 import { AddNoteParams } from '@src/domain/usecases';
 
+const mockAddNoteRepository = (): AddNoteRepository => {
+  class AddNoteRepositoryStub implements AddNoteRepository {
+    async add(parms: AddNoteParams): Promise<Note> {
+      return await Promise.resolve({
+        id: 'any_id',
+        title: 'any_title',
+        description: 'any_description',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+  }
+  return new AddNoteRepositoryStub();
+};
+
+interface SutTypes {
+  sut: DbAddNote;
+  addNoteRepositoryStub: AddNoteRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const addNoteRepositoryStub = mockAddNoteRepository();
+  const sut = new DbAddNote(addNoteRepositoryStub);
+  return { sut, addNoteRepositoryStub };
+};
+
 describe('DbAddNote UseCase', () => {
   it('Should call AddNoteRepository with corret values', async () => {
-    class AddNoteRepositoryStub implements AddNoteRepository {
-      async add(parms: AddNoteParams): Promise<Note> {
-        return await Promise.resolve({
-          id: 'any_id',
-          title: 'any_title',
-          description: 'any_description',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        });
-      }
-    }
-    const addNoteRepositoryStub = new AddNoteRepositoryStub();
+    const { sut, addNoteRepositoryStub } = makeSut();
     const addSpy = jest.spyOn(addNoteRepositoryStub, 'add');
-    const sut = new DbAddNote(addNoteRepositoryStub);
     const params = {
       title: 'any_title',
       description: 'any_sercription',
