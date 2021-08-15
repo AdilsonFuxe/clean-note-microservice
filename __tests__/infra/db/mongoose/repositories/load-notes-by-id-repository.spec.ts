fc78752b9f29f9641c3589326107f512cac6d884
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-15 23:08:26 +0100

    test: ensure NoteMongoRepository return a note on loadById success

--- __tests__/infra/db/mongoose/repositories/load-notes-by-id-repository.spec.ts
+++ __tests__/infra/db/mongoose/repositories/load-notes-by-id-repository.spec.ts
@@ -20,4 +20,20 @@ describe('LoadNotebyIdRepository', () => {
     const note = await sut.loadById(Types.ObjectId().toHexString());
     expect(note).toBeNull();
   });
+
+  it('Should return a note on loadById success', async () => {
+    const savedNote = await NoteMongooseModel.create({
+      title: 'any_title',
+      description: 'any_description',
+    });
+    const sut = new NoteMongoRepository();
+    const note = await sut.loadById(savedNote.id);
+    expect(note).toBeTruthy();
+    expect(note).toEqual(
+      expect.objectContaining({
+        title: 'any_title',
+        description: 'any_description',
+      })
+    );
+  });
 });
