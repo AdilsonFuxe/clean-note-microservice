1f012efc439c8185ae0eef4850bd107d954f14e9
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-15 21:58:01 +0100

    feat: ensure NoteMongoRepository return a list of notes on loadAll success

--- src/infra/db/mongoose/repositories/note-mongo-repository.ts
+++ src/infra/db/mongoose/repositories/note-mongo-repository.ts
@@ -1,13 +1,21 @@
-import { AddNoteRepository } from '@src/data/protocols';
+import { AddNoteRepository, LoadNotesRepository } from '@src/data/protocols';
 import { Note } from '@src/domain/models';
 import { AddNoteParams } from '@src/domain/usecases';
 import { NoteMongooseModel, MongoHelper } from '@src/infra/db/mongoose';
 
-export class NoteMongoRepository implements AddNoteRepository {
+export class NoteMongoRepository
+  implements AddNoteRepository, LoadNotesRepository
+{
   async add(params: AddNoteParams): Promise<Note> {
     const doc = new NoteMongooseModel(params);
     await doc.save();
     const parsedDoc = JSON.parse(JSON.stringify(doc));
     return MongoHelper.serialize(parsedDoc);
+  }
+
+  async loadAll(): Promise<readonly Note[]> {
+    const notes = await NoteMongooseModel.find();
+    const serializedNotes = notes.map(MongoHelper.serialize);
+    return serializedNotes;
   }
 }
