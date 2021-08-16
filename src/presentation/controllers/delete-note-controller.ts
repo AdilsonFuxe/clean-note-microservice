bae174eb33d699aceb9535f70e3cc613d2812ba3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-16 04:14:59 +0100

    feat: ensure DeleteNoteController calls DeleteNote with correct id

--- src/presentation/controllers/delete-note-controller.ts
+++ src/presentation/controllers/delete-note-controller.ts
@@ -4,10 +4,13 @@ import {
   HttpResponse,
 } from '@src/presentation/protocols';
 import { notFounError, serverError } from '@src/presentation/helpers';
-import { LoadNoteById } from '@src/domain/usecases';
+import { DeleteNote, LoadNoteById } from '@src/domain/usecases';
 
 export class DeleteNoteController implements Controller {
-  constructor(private readonly loadNoteById: LoadNoteById) {}
+  constructor(
+    private readonly loadNoteById: LoadNoteById,
+    private readonly deleteNote: DeleteNote
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -15,6 +18,7 @@ export class DeleteNoteController implements Controller {
       if (!note) {
         return notFounError('note');
       }
+      await this.deleteNote.delete(httpRequest.params.id);
       return await Promise.resolve({ statusCode: 200 });
     } catch (error) {
       return serverError(error);
