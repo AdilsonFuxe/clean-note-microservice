11712ccf6f19d274df019cf42024c6f5394d9660
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-16 04:34:32 +0100

    test: ensure delete note  route return 204 on delete note success

--- __tests__/main/routes/delete-note.routes.test.ts
+++ __tests__/main/routes/delete-note.routes.test.ts
@@ -18,4 +18,16 @@ describe('Delete /notes/:id', () => {
       .delete(`/api/v1/notes/${Types.ObjectId().toHexString()}`)
       .expect(404);
   });
+
+  it('Should return 204 on on delete note success', async () => {
+    const savedNote = await NoteMongooseModel.create({
+      title: 'title2',
+      description: 'description2',
+    });
+    await request(app)
+      .delete(`/api/v1/notes/${savedNote.id as string}`)
+      .expect(204);
+    const findNote = await NoteMongooseModel.findById(savedNote.id);
+    expect(findNote).toBeFalsy();
+  });
 });
